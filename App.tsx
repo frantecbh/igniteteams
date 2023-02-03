3538392e60823874b9f50721cc8a86b78654c251
frantecbh <franciscomenezesbh@gmail.com>
2023-02-02 22:10:43 -0300

    configurando thema aplicação

--- App.tsx
+++ App.tsx
@@ -1,14 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
+import { ThemeProvider } from 'styled-components';
+import theme from 'src/theme';
+
+
 import { Groups } from '@screens/Groups';
+
+
+
 
 
 
 export default function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Groups />
-      <StatusBar style="auto" />
-    </>
+    </ThemeProvider>
+
+
   );
 }
 
